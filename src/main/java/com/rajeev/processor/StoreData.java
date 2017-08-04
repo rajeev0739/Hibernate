5f9bff1c0dab10df37cdbdfaf5c4941178129609
rajeev0739 <rajeev0739@gmal.com>
2017-08-04 09:09:21 +0530

    First Working Commit

---
+++ src/main/java/com/rajeev/processor/StoreData.java
@@ -0,0 +1,39 @@
+package com.rajeev.processor;
+
+import org.hibernate.Session;  
+import org.hibernate.SessionFactory;  
+import org.hibernate.Transaction;  
+import org.hibernate.cfg.Configuration;
+
+import com.rajeev.persistent.Employee;  
+  
+public class StoreData {  
+public static void main(String[] args) {  
+      
+    //creating configuration object  
+    Configuration cfg=new Configuration();  
+    cfg.configure("hibernate.cfg.xml");//populates the data of the configuration file  
+      
+    //creating session factory object  
+    SessionFactory factory=cfg.buildSessionFactory();  
+      
+    //creating session object  
+    Session session=factory.openSession();  
+      
+    //creating transaction object  
+    Transaction t=session.beginTransaction();  
+          
+    Employee e1=new Employee();  
+    e1.setId(115);  
+    e1.setFirstName("sonoo");  
+    e1.setLastName("jaiswal");  
+      
+    session.persist(e1);//persisting the object  
+      
+    t.commit();//transaction is committed  
+    session.close();  
+      
+    System.out.println("successfully saved");  
+      
+}  
+}  
